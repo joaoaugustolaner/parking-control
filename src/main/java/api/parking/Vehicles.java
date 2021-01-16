62945ad1c01f765ee9ad35f0a21e5c978b93c940
João Augusto Tonial Laner <joaoaugustolaner@gmail.com>
2021-01-16 18:52:00 -0300

    Creating entities

---
+++ src/main/java/api/parking/Vehicles.java
@@ -0,0 +1,13 @@
+package api.parking;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public enum Vehicles {
+
+    TRUCK,
+    BUS,
+    PICK_UP,
+    CAR,
+    BIKE
+}
---
+++ src/main/java/api/parking/entity/Space.java
@@ -0,0 +1,24 @@
+package api.parking.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Space{
+
+    private int column;
+
+    private int row;
+
+    private boolean isOccupied;
+
+    private boolean preferred;
+
+    private boolean mensalist;
+}
